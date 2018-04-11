e3cfa87dc65dbadb3102b862842abc1a0ff76eee
Tom Sullivan <tom@msbit.com.au>
2018-04-11 16:41:32 +1000

    Move Raytracer#map to use ranges

--- raytracer.rb
+++ raytracer.rb
@@ -7,16 +7,16 @@ class RayTracer
                                        min_depth, max_depth)
   end
 
-  def map(input_min, input_max, output_min, output_max, input)
-    scale_factor = (output_max - output_min) / (input_max - input_min)
-    input * scale_factor + output_min
+  def map(input_range, output_range, input)
+    scale_factor = (output_range.max - output_range.min) / (input_range.max - input_range.min)
+    input * scale_factor + output_range.min
   end
 
   def precalc_depths(min_depth_delta, max_depth_delta, min_depth, max_depth)
     depths = [min_depth_delta]
     depth = min_depth_delta
     until depth >= max_depth
-      depth += map(min_depth, max_depth, min_depth_delta, max_depth_delta, depth)
+      depth += map((min_depth..max_depth), (min_depth_delta..max_depth_delta), depth)
       depths << depth
     end
     depths
